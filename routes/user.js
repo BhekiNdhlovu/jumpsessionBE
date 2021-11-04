b5a8caf45e6bf81d65320703bd2bd0383f1cea45
pmotsumi <Pmotsumi@retrorabbit.co.za>
2021-11-04 16:16:57 +0200

    Added register user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
-router.post('/register', (req, res) => {
-    return res.json('Testing register')
+const { User } = require('../models')
+
+router.post('/register', async (req, res) => {
+    try {
+        const existingUserName = await User.findOne({ where: { username: req.body.username } })
+        const existingUserEmail = await User.findOne({ where: { email: req.body.email } })
+        if(existingUserName && (existingUserName.username == req.body.username)) return res.json("Username already taken")
+        if(existingUserEmail && (existingUserEmail.email == req.body.email)) return res.json("User already exists")
+
+        const hash = bcrypt.hashSync(req.body.password, 10)
+        await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+        })
+
+        return res.json('User successfully registered')
+    } catch (err) {
+        return res.status(400).json({ error: err})
+    }
 })
 
 router.post('/login', (req, res) => {
