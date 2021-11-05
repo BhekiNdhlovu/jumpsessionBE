88d61604a605cc34bd604e1590802d12d3fa8d2f
pmotsumi <Pmotsumi@retrorabbit.co.za>
2021-11-05 11:09:36 +0200

    Added course

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const userRouter = require('./routes/user')
 const courseRouter = require('./routes/course')
@@ -13,6 +14,7 @@ const db = require('./models/index');
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 app.use(cookieParser())
 
