88f5411983e3f5e685a700f1c431e948c5b3a708
pmotsumi <Pmotsumi@retrorabbit.co.za>
2021-11-06 08:07:56 +0200

    search route update

--- routes/course.js
+++ routes/course.js
@@ -30,14 +30,22 @@ router.get('/get-categories', async (req, res) => {
     }
 })
 
-// Search Course Route
+// Search Course by name, category, instructor Route
 router.post('/search-course', async (req, res) => {
    try {
        const courses = await Course.findAll({
            where: {
-                course_name : {
-                    [Op.like]: `%${req.body.searchString}%`
-                }
+                [Op.or]: [
+                   { course_name : {
+                        [Op.like]: `%${req.body.searchString}%`
+                    }},
+                    {instructor : {
+                        [Op.like]: `%${req.body.searchString}%`
+                    }},
+                    {category : {
+                        [Op.like]: `%${req.body.searchString}%`
+                    }}
+                ]
             },
             offset: req.body.count,
             limit: req.body.limit
@@ -48,63 +56,6 @@ router.post('/search-course', async (req, res) => {
    } catch (err) {
        return res.status(400).json({ error: err})
    }
-})
-
-// Find Course by Category
-router.post('/get-course-by-category', async (req, res) => {
-    try {
-        const courses = await Course.findAll({
-            where: {
-                category : {
-                     [Op.like]: `%${req.body.category}%`
-                 }
-             },
-             offset: req.body.count,
-             limit: req.body.limit
-         })
-        if(courses.length > 0) return res.json(courses)
- 
-        return res.json(`No courses found for ${req.body.category}`)
-    } catch (err) {
-        return res.status(400).json({ error: err})
-    }
-})
-
-// Find Course by Instructor
-router.post('/get-course-by-instructor', async (req, res) => {
-    try {
-        const courses = await Course.findAll({
-            where: {
-                instructor : {
-                     [Op.like]: `%${req.body.instructor}%`
-                 }
-             },
-             offset: req.body.count,
-             limit: req.body.limit
-         })
-        if(courses.length > 0) return res.json(courses)
- 
-        return res.json(`No courses found by ${req.body.instructor}`)
-    } catch (err) {
-        return res.status(400).json({ error: err})
-    }
-})
-
-// New Course Route
-router.post('/new-course', async (req, res) => {
-    try {
-        const course = await Course.create({
-            instructor: req.body.instructor,
-            course_name: req.body.course_name,
-            description: req.body.description,
-            category: req.body.category,
-            price: req.body.price
-        })
-
-        return res.json(course)
-    } catch (error) {
-        return res.status(400).json('Invalid input')
-    }
 })
 
 module.exports = router
